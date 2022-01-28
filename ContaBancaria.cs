f1c8a46644d6fbb1613910fc6fc51bec82bb110b
Thiago Mateus <thiagomateus123@hotmail.com>
2022-01-28 15:54:47 -0300

    Criação do Programa

---
+++ ContaBancaria.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioPOOCadastrarContaBancaria
+{
+    public class ContaBancaria
+    {
+        public int NumeroDaConta { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(int numeroDaConta, string titular)
+        {
+            NumeroDaConta = numeroDaConta;
+            Titular = titular;
+            Saldo = 0;
+        }
+
+        public ContaBancaria(double depositoInicial, int numeroDaConta, string titular) : this(numeroDaConta,titular)
+        {
+            Deposito(depositoInicial);
+        }
+
+        public void Deposito(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Saque(double valor)
+        {
+            Saldo -= (valor + 5.0);
+        }
+
+        public override string ToString()
+        {
+            return $"Conta {NumeroDaConta}, Titular: {Titular}, Saldo: $ {Saldo}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using ExercicioPOOCadastrarContaBancaria;
+
+ContaBancaria conta;
+
+Console.WriteLine("Entre o número da Conta: ");
+int numeroConta = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Entre o titular da conta: ");
+string titular = Console.ReadLine();
+
+Console.WriteLine("Haverá deposito inicial? (s/n)");
+char opcao = char.Parse(Console.ReadLine());
+
+if (opcao == 's')
+{
+    Console.WriteLine("Entre o valor do depósito");
+    double valor = double.Parse(Console.ReadLine());
+    conta = new ContaBancaria(valor, numeroConta, titular);
+}
+else
+{
+    conta = new ContaBancaria(numeroConta, titular);
+}
+
+Console.WriteLine("\nDados da Conta:");
+Console.WriteLine(conta);
+
+Console.WriteLine("\nEntre um valor para deposito: ");
+conta.Deposito(double.Parse(Console.ReadLine()));
+
+Console.WriteLine("\nDados da conta atualizados: ");
+Console.WriteLine(conta);
+
+Console.WriteLine("\nEntre um valor de saque:");
+conta.Saque(double.Parse(Console.ReadLine()));
+
+Console.WriteLine("\nDados da conta atualizado:");
+Console.WriteLine(conta);
